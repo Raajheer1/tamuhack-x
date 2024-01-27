9697f1512ec685413d6db46b9b129b3d5ae40cb0
Raajheer1 <raaj.patel229@gmail.com>
2024-01-27 14:40:11 -0600

    Fix main

--- backend/cmd/main.go
+++ backend/cmd/main.go
@@ -1,13 +1,19 @@
-package cmd
+package main
 
 import (
 	"github.com/Raajheer1/tamuhack-x/m/v2/pkg/config"
 	"github.com/Raajheer1/tamuhack-x/m/v2/pkg/database"
+	"github.com/Raajheer1/tamuhack-x/m/v2/pkg/database/models"
+	"log"
 )
 
 func main() {
 	cfg := config.New()
 
 	db := database.Connect(cfg.Database)
-	models.AutoMigrate(db)
+	err := models.AutoMigrate(db)
+	if err != nil {
+		log.Fatalf("[Database] Migration Error: %s", err)
+	}
+
 }
