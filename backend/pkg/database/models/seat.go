1b8ded9061108d383be605422046f557cb888c88
Raajheer1 <raaj.patel229@gmail.com>
2024-01-28 05:16:48 -0600

    Connect everything

--- backend/pkg/database/models/seat.go
+++ backend/pkg/database/models/seat.go
@@ -60,6 +60,16 @@ func GetAllSeatsWithAccountID(db *gorm.DB, accountID string) ([]*Seat, error) {
 	return seats, err
 }
 
+func GetAllSeatsWithFlightID(db *gorm.DB, flightID uint) ([]*Seat, error) {
+	var seats []*Seat
+	err := db.Where("flight_id = ?", flightID).Find(&seats).Error
+	if err != nil {
+		return nil, err
+	}
+
+	return seats, err
+}
+
 func UpdateSeat(db *gorm.DB, seat *Seat) error {
 	err := db.Save(seat).Error
 	if err != nil {
--- frontend/AASE/src/components/Home.vue
+++ frontend/AASE/src/components/Home.vue
@@ -10,7 +10,7 @@
         <h1 class="text-4xl text-white font-semibold mt-8 tracking-wider">Your next adventure awaits</h1>
       </div>
       <div class="max-h-screen overflow-auto bg-white rounded-t-3xl py-10 px-6 inner">
-        <div class="flex flex-col" v-for="seat in seats">
+        <div class="flex flex-col" v-for="seat in props.seats">
           <div :key="seat.id" @click="clicked(seat.id.toString())">
             <SeatCard :seat="seat" />
           </div>
@@ -21,8 +21,6 @@
 </template>
 
 <script setup lang="ts">
-import {Seat} from "../types";
-import { ref } from 'vue';
 import SeatCard from "./SeatCard.vue";
 
 const emit = defineEmits<{
@@ -33,88 +31,90 @@ const clicked = (id: string) => {
   emit('seat', id)
 }
 
-const seats = ref<Seat[]>([
-  {
-    id: 1,
-    seat_number: "1A",
-    flight: {
-      id: 1,
-      origin: "SEA",
-      destination: "ORD",
-      aircraft_type: "737",
-      schedule_departure_time: "2021-10-10T12:00:00.000Z",
-      schedule_arrival_time: "2021-10-10T18:00:00.000Z",
-    },
-    assigned_pnr: "ABCDEF",
-    bid_time_end: new Date().toString(),
-    buy_now_price: 0,
-    current_bid: 0,
-    flight_id: 0,
-    for_sale: false,
-    last_bidder: "",
-    min_price: 0,
-  },
-  {
-    id: 2,
-    seat_number: "3B",
-    flight: {
-      id: 66,
-      origin: "DFW",
-      destination: "NYC",
-      aircraft_type: "737",
-      schedule_departure_time: "2021-10-10T12:00:00.000Z",
-      schedule_arrival_time: "2021-10-10T18:00:00.000Z",
-    },
-    assigned_pnr: "DEFGFF",
-    bid_time_end: new Date().toString(),
-    buy_now_price: 0,
-    current_bid: 0,
-    flight_id: 0,
-    for_sale: false,
-    last_bidder: "",
-    min_price: 0,
-  },
-  {
-    id: 3,
-    seat_number: "11F",
-    flight: {
-      id: 46,
-      origin: "IAH",
-      destination: "CLL",
-      aircraft_type: "737",
-      schedule_departure_time: "2021-10-10T12:00:00.000Z",
-      schedule_arrival_time: "2021-10-10T18:00:00.000Z",
-    },
-    assigned_pnr: "DEFGFF",
-    bid_time_end: new Date().toString(),
-    buy_now_price: 0,
-    current_bid: 0,
-    flight_id: 0,
-    for_sale: false,
-    last_bidder: "",
-    min_price: 0,
-  },
-  {
-    id: 4,
-    seat_number: "21F",
-    flight: {
-      id: 46,
-      origin: "MIA",
-      destination: "SFO",
-      aircraft_type: "737",
-      schedule_departure_time: "2021-10-10T12:00:00.000Z",
-      schedule_arrival_time: "2021-10-10T18:00:00.000Z",
-    },
-    assigned_pnr: "DEFGFF",
-    bid_time_end: new Date().toString(),
-    buy_now_price: 0,
-    current_bid: 0,
-    flight_id: 0,
-    for_sale: false,
-    last_bidder: "",
-    min_price: 0,
-  }
-]);
+const props = defineProps(['seats'])
+
+// const seats = ref<Seat[]>([
+//   {
+//     id: 1,
+//     seat_number: "1A",
+//     flight: {
+//       id: 1,
+//       origin: "SEA",
+//       destination: "ORD",
+//       aircraft_type: "737",
+//       schedule_departure_time: "2021-10-10T12:00:00.000Z",
+//       schedule_arrival_time: "2021-10-10T18:00:00.000Z",
+//     },
+//     assigned_pnr: "ABCDEF",
+//     bid_time_end: new Date().toString(),
+//     buy_now_price: 0,
+//     current_bid: 0,
+//     flight_id: 0,
+//     for_sale: false,
+//     last_bidder: "",
+//     min_price: 0,
+//   },
+//   {
+//     id: 2,
+//     seat_number: "3B",
+//     flight: {
+//       id: 66,
+//       origin: "DFW",
+//       destination: "NYC",
+//       aircraft_type: "737",
+//       schedule_departure_time: "2021-10-10T12:00:00.000Z",
+//       schedule_arrival_time: "2021-10-10T18:00:00.000Z",
+//     },
+//     assigned_pnr: "DEFGFF",
+//     bid_time_end: new Date().toString(),
+//     buy_now_price: 0,
+//     current_bid: 0,
+//     flight_id: 0,
+//     for_sale: false,
+//     last_bidder: "",
+//     min_price: 0,
+//   },
+//   {
+//     id: 3,
+//     seat_number: "11F",
+//     flight: {
+//       id: 46,
+//       origin: "IAH",
+//       destination: "CLL",
+//       aircraft_type: "737",
+//       schedule_departure_time: "2021-10-10T12:00:00.000Z",
+//       schedule_arrival_time: "2021-10-10T18:00:00.000Z",
+//     },
+//     assigned_pnr: "DEFGFF",
+//     bid_time_end: new Date().toString(),
+//     buy_now_price: 0,
+//     current_bid: 0,
+//     flight_id: 0,
+//     for_sale: false,
+//     last_bidder: "",
+//     min_price: 0,
+//   },
+//   {
+//     id: 4,
+//     seat_number: "21F",
+//     flight: {
+//       id: 46,
+//       origin: "MIA",
+//       destination: "SFO",
+//       aircraft_type: "737",
+//       schedule_departure_time: "2021-10-10T12:00:00.000Z",
+//       schedule_arrival_time: "2021-10-10T18:00:00.000Z",
+//     },
+//     assigned_pnr: "DEFGFF",
+//     bid_time_end: new Date().toString(),
+//     buy_now_price: 0,
+//     current_bid: 0,
+//     flight_id: 0,
+//     for_sale: false,
+//     last_bidder: "",
+//     min_price: 0,
+//   }
+// ]);
 
 
 </script>
