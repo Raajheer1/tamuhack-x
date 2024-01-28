af6c2d02fa6c75e3c52a9da0914f5ea57975ae40
lennytanui <lennytanui@outlook.com>
2024-01-28 01:47:10 -0600

    misc changes

--- frontend/AASE/src/components/Account.vue
+++ frontend/AASE/src/components/Account.vue
@@ -1,20 +1,41 @@
 <template>
     <div class="w-120 h-32 bg-aa-blue rounded-b-3xl">
         <div class="flex">
-            <div>
+            <div class="flex-1">
                 <h1>7:00 AM</h1> 
                 <h2>1/28/24</h2>
             </div>
 
-            <div>
+            <div class="flex-1">
                 <h1>SEA</h1>
                 <h2>Seatle</h2>
             </div>
 
-            <div>
-                <img src="../assets/american_logo.png" alt="">
+            <div class="flex-1.2">
+                <div>
+                    <img class="size-16" src="../assets/american_logo.png" alt="">
+                </div>
+                <div>
+                    <h1 class="text-center font-bold text-xl">Seat 23</h1>
+                </div>
             </div>
 
+            <div class="flex-1">
+                <h1>ORD</h1>
+                <h2>Chicago</h2>
+            </div>
+
+            <div class="flex-1">
+                <h1>10:00 AM</h1>
+                <h2>1/28/24</h2>
+            </div>
         </div>
     </div>
 </template>
+
+<style>
+    div.flex div{
+        padding: 0.3em;
+        font-size: 0.9em;
+    }
+</style>
